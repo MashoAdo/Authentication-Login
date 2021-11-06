670a3d4f83baa2f24a1f057b1f8ec7a97f12a822
Masho Ado <mashoado@gmail.com>
2021-11-06 13:15:13 +0300

    connect to Atlas a cloud mongoDB service

--- app.js
+++ app.js
@@ -5,12 +5,20 @@ const mongoose = require("mongoose")
 const passport = require("passport")
 const localStrategy = require("passport-local").Strategy
 const bcrypt = require("bcrypt")
+const { MongoClient } = require('mongodb')
+require("dotenv").config()
 
 const app = express()
-
-
 const PORT = process.env.PORT || "8000"
+// conncet to cloud mongo db
+const uri = `mongodb+srv://masho:<${process.env.MONGODB_pass}>@xtechguy.jstjm.mongodb.net/AuthenticationApp?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 app.listen(PORT, () => {
-    console.log("server is running on"{ PORT })
+    console.log(`server is up and running on PORT: ${PORT}`)
 })
