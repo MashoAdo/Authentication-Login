a6f12468c34840e5f0580dd8cb875866097abf4f
Masho Ado <mashoado@gmail.com>
2021-11-06 21:39:02 +0300

    add middlewares including, setting engine, static,bodyparser and for session management

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const session = require("express-session")
-const exphbs = require("express-handlebars")
+const hbs = require("express-handlebars")
 const mongoose = require("mongoose")
 const passport = require("passport")
 const localStrategy = require("passport-local").Strategy
@@ -12,13 +12,42 @@ const PORT = process.env.PORT || "8000"
 
 
 // conncet to cloud mongo db
-mongoose.connect(`mongodb+srv://masho:${process.env.MONGODB_PASS}@xtechguy.jstjm.mongodb.net/Authentication?retryWrites=true&w=majority`, {
+mongoose.connect(`mongodb+srv://masho:${process.env.MONGODB_PASS}@xtechguy.jstjm.mongodb.net/xtechguy?retryWrites=true&w=majority`, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
 mongoose.connection.once("open", () => {
   console.log('connected to database')
 })
+
+
+// middleware
+app.engine("hbs", hbs({extname: '.hbs'}))
+app.set("view engine", 'hbs')
+app.use(express.static(__dirname + "/public"))
+app.use(session({
+  secret: "verysolidsecret",
+  resave: false,
+  saveUninitialized: true
+
+}))
+// for parsing
+app.use(express.urlencoded({extended: false}))
+// for testing
+app.use(express.json())
+
+
+//====== passport.js==========
+// app.use(passport.initialize())
+// // to keep the session running
+// app.use(passport.session())
+
+// app.serializeUSer
+
+
+
+
+
 
 
 app.listen(PORT, () => {
