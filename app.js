ac8fa6865bcb131b7d7b027a1e809e34839ab95f
Masho Ado <mashoado@gmail.com>
2021-11-06 13:36:41 +0300

    use mongoose to connect to cloud db instead of mongoclient

--- app.js
+++ app.js
@@ -5,20 +5,22 @@ const mongoose = require("mongoose")
 const passport = require("passport")
 const localStrategy = require("passport-local").Strategy
 const bcrypt = require("bcrypt")
-const { MongoClient } = require('mongodb')
 require("dotenv").config()
 
 const app = express()
 const PORT = process.env.PORT || "8000"
+
+
 // conncet to cloud mongo db
-const uri = `mongodb+srv://masho:<${process.env.MONGODB_pass}>@xtechguy.jstjm.mongodb.net/AuthenticationApp?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+mongoose.connect(`mongodb+srv://masho:${process.env.MONGODB_PASS}@xtechguy.jstjm.mongodb.net/Authentication?retryWrites=true&w=majority`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+mongoose.connection.once("open", () => {
+  console.log('connected to database')
+})
+
 
 app.listen(PORT, () => {
-    console.log(`server is up and running on PORT: ${PORT}`)
+  console.log(`server is up and running on PORT: ${PORT}`)
 })
